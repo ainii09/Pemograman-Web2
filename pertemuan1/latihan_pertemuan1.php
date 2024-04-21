bc362449b52a71297af1f6618d22207386c05cfd
Khurotul_Nuraini <86713197+ainii09@users.noreply.github.com>
2024-04-21 12:09:37 +0700

    Add files via upload

---
+++ pertemuan1/latihan_pertemuan1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BIODATA</title>
+</head>
+<body>
+<?php 
+    print"<pre>";
+    print"<center><font size= 12 color='red'><b> BIODATA </b></font></center>"; 
+    
+    print"<pre>";
+    print"<font size= 8 color='blue'>Nama      :<b> Khurotul Nuraini </b></font>"; 
+    
+    print"<pre>";
+    print"<font size= 10 color='blue'>Email     : khurotulnuraini20@gmail.com</font>"; 
+    
+    print"<pre>";
+    print"<font size= 10 color='blue'>Jurusan   : Teknik Informatika </font>"; 
+    
+    print"<pre>";
+    print"<font size= 10 color='blue'>Hoby      : Memasak <font><br>";  
+  ?>
+  
+  
+</body>
+</html>
